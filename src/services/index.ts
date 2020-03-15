75c58abb124afc756dba0d5d90a173d4f4b0d0e3
yogendrasri <ysrivastava@in.ibm.com>
2020-03-15 19:56:20 +0530

    Initial commit

---
+++ src/services/index.ts
@@ -0,0 +1,6 @@
+export * from './project.api';
+export * from './project.service';
+export * from './task.api';
+export * from './task.service';
+export * from './stock-items.api';
+export * from './stock-items.service';
